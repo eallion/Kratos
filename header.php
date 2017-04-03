7f8edcad961b076137e0e5d79bceeb4685b3284a
Charles Chin <eallion@eallion.com>
2017-04-03 13:55:37 +0800

    静态化

--- header.php
+++ header.php
@@ -2,25 +2,21 @@
 <!DOCTYPE HTML>
 <html class="no-js">
 	<head>
-		<title><?php $this->archiveTitle(array('category'=>_t(' %s '),'search'=>_t(' %s '),'tag'=>_t(' %s '),'author'=>_t(' %s ')),'',' - ');?> <?php $this->options->title();?></title>
-		<meta charset="<?php $this->options->charset(); ?>">
+		<title><?php $this->archiveTitle(array('category'=>_t(' %s '),'search'=>_t(' %s '),'tag'=>_t(' %s '),'author'=>_t(' %s ')),'',' - ');?> eallion</title>
+		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta http-equiv="Cache-Control" content="no-transform" />  
-        <meta http-equiv="Cache-Control" content="no-siteapp" />  
+		<meta http-equiv="Cache-Control" content="no-transform" />  
+		<meta http-equiv="Cache-Control" content="no-siteapp" />  
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<meta name="keywords" content="<?php $this->keywords() ?>" />
+		<meta name="keywords" content="eallion,snail,woniu,蜗牛,大大的小蜗牛,<?php $this->keywords() ?>" />
 		<link rel="shortcut icon" href="//eallion.com/favicon.ico">
 		<link rel='stylesheet' id='animate-style-css'  href='//lib.baomitu.com/animate.css/3.5.1/animate.min.css' type='text/css' media='all' />
 		<link rel='stylesheet' id='awesome-style-css'  href='//lib.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
 		<link rel='stylesheet' id='bootstrap-style-css'  href='//lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css' type='text/css' media='all' />
 		<link rel='stylesheet' id='superfish-style-css'  href='//lib.baomitu.com/superfish/1.7.4/superfish.min.css' type='text/css' media='all' />
-		<link rel='stylesheet' id='kratos-style-css'  href='<?php $this->options->themeUrl('style.css?ver=2.5.2'); ?>' type='text/css' media='all' />
-		<link rel='stylesheet' id='kratos-diy-style-css'  href='<?php $this->options->themeUrl('css/kratos.diy.css?ver=2.5.2'); ?>' type='text/css' media='all' />
+		<link rel='stylesheet' id='kratos-style-css'  href='//getcdn.org/eallion/themes/Kratos/style.css?ver=2.5.2' type='text/css' media='all' />
+		<link rel='stylesheet' id='kratos-diy-style-css'  href='//getcdn.org/eallion/themes/Kratos/css/kratos.diy.css?ver=2.5.2' type='text/css' media='all' />
 		<?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
-		<?php if($this->options->site_bw == 'able'): ?>
-			<style type="text/css">html{filter: grayscale(100%);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);filter: gray;-webkit-filter: grayscale(1); }
-			</style>
-		<?php endif; ?>
 	</head>
 	<?php flush(); ?>
 	<body data-spy="scroll" data-target=".scrollspy">
@@ -31,29 +27,24 @@
 						<div class="container">
 							<div class="nav-header">
 								<a href="#" class="js-kratos-nav-toggle kratos-nav-toggle"><i></i></a>
-								<?php if ( !empty( $this->options->logoUrl ) ) {?>
-									<a href="<?php $this->options ->siteUrl(); ?>">
-									<h1 id="kratos-logo-img"><img src="<?php $this->options->logoUrl(); ?>"></h1>
-									</a>
-								<?php }else{?>
-									<h1 id="kratos-logo"><a href="<?php $this->options ->siteUrl(); ?>"><?php $this->options->title();?></a></h1>
-								<?php }?>
+									<h1 id="kratos-logo"><a href="https://eallion.com/">大大的小蜗牛</a></h1>
 								<nav id="kratos-menu-wrap" class="menu-container">
 									<ul id="kratos-primary-menu" class="sf-menu">
-										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">Home</a></li>
-										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">分类</a>
+										<li class="current-menu-item"><a href="https://eallion.com/">Home</a></li>
+										<li class="current-menu-item"><a href="https://eallion.com/">分类</a>
 											<ul class="sub-menu">
-												<?php $this->widget('Widget_Metas_Category_List')->to($cats); ?>
-												<?php while ($cats->next()): ?>
-												<li><a href="<?php $cats->permalink()?>"><?php $cats->name()?></a></li>
-												<?php endwhile; ?>
+												<li><a href="https://eallion.com/category/article">嘀咕</a></li>
+												<li><a href="https://eallion.com/category/sz">山贼</a></li>
+												<li><a href="https://eallion.com/category/share">分享</a></li>
+												<li><a href="https://eallion.com/category/photo">图片</a></li>
+												<li><a href="https://eallion.com/category/code">代码</a></li>
 											</ul>
 										</li>
 										<li class="current-menu-item">
-												<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-												<?php while($pages->next()): ?>
-												<li><a href="<?php $pages->permalink(); ?>"><?php $pages->title(); ?></a></li>
-												<?php endwhile; ?>
+											<li><a href="https://eallion.com/archive">归档</a></li>
+											<li><a href="https://eallion.com/guestbook">留言板</a></li>
+											<li><a href="https://eallion.com/links">友链</a></li>
+											<li><a href="https://eallion.com/about">关于</a></li>
 										</li>
 									</ul>
 								</nav>
@@ -61,10 +52,10 @@
 						</div>
 					</header>
 				</div>
-<div class="kratos-start kratos-hero-2">
+	<div class="kratos-start kratos-hero-2">
 	<div class="kratos-overlay"></div>
-	<div class="kratos-cover kratos-cover_2 text-center" style="background-image: url(<?php ($this->options->bannerimg) ? $this->options->bannerimg() : $this->options->themeUrl('images/background.jpg'); ?>);">
-		<div class="desc desc2 animate-box"><h2><?php $this->options->logoTxt(); ?></h2><span><?php hellobingbing("geyan"); ?><!--随机格言--></span></div>
+	<div class="kratos-cover kratos-cover_2 text-center" style="background-image: url(//getcdn.org/eallion/themes/Kratos/images/background.jpg);">
+		<div class="desc desc2 animate-box"><span><?php hellobingbing("geyan"); ?></span></div>
 	</div>
 </div>
 <div id="kratos-blog-post" style="background:#f5f5f5"><!--header-->
