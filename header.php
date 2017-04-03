d7dd6d820568eeb67a4afa7ad3501cf5d8a08863
Charles Chin <eallion@eallion.com>
2017-04-03 12:15:01 +0800

    修改header菜单，修改为360前端库CDN

--- header.php
+++ header.php
@@ -9,11 +9,11 @@
         <meta http-equiv="Cache-Control" content="no-siteapp" />  
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<meta name="keywords" content="<?php $this->keywords() ?>" />
-		<link rel="shortcut icon" href="<?php $this->options->themeUrl('favicon.ico'); ?>">
-		<link rel='stylesheet' id='animate-style-css'  href='//cdn.bootcss.com/animate.css/3.5.1/animate.min.css' type='text/css' media='all' />
-		<link rel='stylesheet' id='awesome-style-css'  href='//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
-		<link rel='stylesheet' id='bootstrap-style-css'  href='//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css' type='text/css' media='all' />
-		<link rel='stylesheet' id='superfish-style-css'  href='//cdn.bootcss.com/superfish/1.7.4/superfish.min.css' type='text/css' media='all' />
+		<link rel="shortcut icon" href="//eallion.com/favicon.ico">
+		<link rel='stylesheet' id='animate-style-css'  href='//lib.baomitu.com/animate.css/3.5.1/animate.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' id='awesome-style-css'  href='//lib.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' id='bootstrap-style-css'  href='//lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' id='superfish-style-css'  href='//lib.baomitu.com/superfish/1.7.4/superfish.min.css' type='text/css' media='all' />
 		<link rel='stylesheet' id='kratos-style-css'  href='<?php $this->options->themeUrl('style.css?ver=2.5.2'); ?>' type='text/css' media='all' />
 		<link rel='stylesheet' id='kratos-diy-style-css'  href='<?php $this->options->themeUrl('css/kratos.diy.css?ver=2.5.2'); ?>' type='text/css' media='all' />
 		<?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
@@ -41,7 +41,7 @@
 								<nav id="kratos-menu-wrap" class="menu-container">
 									<ul id="kratos-primary-menu" class="sf-menu">
 										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">Home</a></li>
-										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">Categories</a>
+										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">分类</a>
 											<ul class="sub-menu">
 												<?php $this->widget('Widget_Metas_Category_List')->to($cats); ?>
 												<?php while ($cats->next()): ?>
@@ -49,13 +49,11 @@
 												<?php endwhile; ?>
 											</ul>
 										</li>
-										<li class="current-menu-item"><a href="<?php $this->options ->siteUrl(); ?>">Pages</a>
-											<ul class="sub-menu">
+										<li class="current-menu-item">
 												<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
 												<?php while($pages->next()): ?>
 												<li><a href="<?php $pages->permalink(); ?>"><?php $pages->title(); ?></a></li>
 												<?php endwhile; ?>
-											</ul>
 										</li>
 									</ul>
 								</nav>
@@ -66,7 +64,7 @@
 <div class="kratos-start kratos-hero-2">
 	<div class="kratos-overlay"></div>
 	<div class="kratos-cover kratos-cover_2 text-center" style="background-image: url(<?php ($this->options->bannerimg) ? $this->options->bannerimg() : $this->options->themeUrl('images/background.jpg'); ?>);">
-		<div class="desc desc2 animate-box"><h2><?php $this->options->logoTxt(); ?></h2><span><?php $this->options->description();?></span></div>
+		<div class="desc desc2 animate-box"><h2><?php $this->options->logoTxt(); ?></h2><span><?php hellobingbing("geyan"); ?></span></div>
 	</div>
 </div>
 <div id="kratos-blog-post" style="background:#f5f5f5"><!--header-->
