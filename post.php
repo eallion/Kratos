b6fad17189ef0ba56dfe172ae63efb8349586bf5
Charles Chin <eallion@eallion.com>
2017-04-03 14:34:02 +0800

    静态化

--- post.php
+++ post.php
@@ -9,21 +9,14 @@
 <?php $this->need('header.php'); ?>
 	<div class="container">
 		<div class="row">
-			<?php if ($this->options->sidebarlr == 'left_side'): ?>
-			<aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
-                <div id="sidebar">
-                    <?php $this->need('sidebar.php'); ?>
-                </div>
-            </aside>
-			<?php endif; ?>
-            <section id="main" class='<?php echo ($this->options->sidebarlr ==  'single') ? 'col-md-12' : 'col-md-8'; ?>'>
+            <section id="main" class='col-md-8'>
 				<article>
 					<div class="kratos-hentry kratos-post-inner clearfix">
 						<header class="kratos-entry-header">
 							<h1 class="kratos-entry-title text-center"><?php $this->title(); ?></h1>
 							<div class="kratos-post-meta text-center">
 								<span>
-								<i class="fa fa-calendar"></i> <?php $this->date('Y-m-d'); ?>
+								<i class="fa fa-calendar"></i> <?php $this->date('F j, Y'); ?>
 				                <i class="fa fa-commenting-o"></i> <?php $this->commentsNum('0', '1', '%d'); ?> Comments
 				                <i class="fa fa-eye"></i> <?php get_post_view($this); ?> Views
 								</span>
@@ -43,7 +36,7 @@
 						</footer>
 					</div>
 					<div class="kratos-hentry kratos-copyright text-center clearfix">
-						<img alt="知识共享许可协议" src="<?php $this->options->themeUrl('images/licenses.png'); ?>">
+						<img alt="知识共享许可协议" src="<?php $this->options->themeUrl('//getcdn.org/eallion/themes/Kratos/images/licenses.png'); ?>">
 						<h5>本作品采用 <a rel="license nofollow" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a> 进行许可</h5>
 					</div>
 					<nav class="navigation post-navigation clearfix" role="navigation">
@@ -57,13 +50,11 @@
 					<?php $this->need('comments.php'); ?>
 				</article>
 			</section>
-			<?php if ($this->options->sidebarlr == 'right_side'): ?>
 				<aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
 	                <div id="sidebar">
 	                    <?php $this->need('sidebar.php'); ?>
 	                </div>
 	            </aside>
-			<?php endif; ?>
 		</div>
 	</div>
 </div>
