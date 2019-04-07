b1af4e50b39d7a48a55e3b42669fb919063eff5a
Charles Chin <eallion@eallion.com>
2019-04-08 01:48:52 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
 <head>
-	<title>404 - 大大的小蜗牛</title>
+	<title>大大的小蜗牛 - 宝贝回家 - 腾讯公益404</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta http-equiv="Cache-Control" content="no-transform" />  
 	<meta http-equiv="Cache-Control" content="no-siteapp" />  
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta name="keywords" content="eallion,snail,woniu,蜗牛,大大的小蜗牛,eallion,snail,woniu,蜗牛,大大的小蜗牛" />
-	<meta name="description" content="大大的小蜗牛" />
-	<meta name="generator" content="Typecho 1.0/14.10.10" />
+	<meta name="description" content="大大的小蜗牛 腾讯公益404 宝贝回家 贡献我的微薄之力" />
 	<meta name="template" content="Kratos" />
-	<link rel="dns-prefetch" href="//cdn.eallion.com" />
-	<link rel="dns-prefetch" href="//tajs.qq.com" />
 	<link rel="shortcut icon" href="//cdn.eallion.com/eallion/favicon.ico">
 </head>
 <body>
